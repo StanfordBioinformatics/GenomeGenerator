7a69e05cd18f23c1f291f725b249df65e0c31709
Greg <gregorymcinnes@gmail.com>
2016-02-21 21:06:58 -0800

    now emits singletons with proper titv ratio

--- bq-genome-generator/src/main/java/com/stanford/dataflow/GenomeGenerator.java
+++ bq-genome-generator/src/main/java/com/stanford/dataflow/GenomeGenerator.java
@@ -18,31 +18,21 @@ package com.stanford.dataflow;
 
 import com.google.cloud.dataflow.sdk.Pipeline;
 import com.google.cloud.dataflow.sdk.options.PipelineOptionsFactory;
-import com.google.cloud.dataflow.sdk.transforms.Create;
 import com.google.cloud.dataflow.sdk.transforms.DoFn;
 import com.google.cloud.dataflow.sdk.transforms.ParDo;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.google.api.services.bigquery.model.TableFieldSchema;
 import com.google.api.services.bigquery.model.TableRow;
 import com.google.api.services.bigquery.model.TableSchema;
-import com.google.cloud.dataflow.sdk.Pipeline;
 import com.google.cloud.dataflow.sdk.io.BigQueryIO;
 import com.google.cloud.dataflow.sdk.options.Default;
 import com.google.cloud.dataflow.sdk.options.Description;
 import com.google.cloud.dataflow.sdk.options.PipelineOptions;
-import com.google.cloud.dataflow.sdk.options.PipelineOptionsFactory;
-import com.google.cloud.dataflow.sdk.options.Validation;
-import com.google.cloud.dataflow.sdk.transforms.Count;
-import com.google.cloud.dataflow.sdk.transforms.DoFn;
 import com.google.cloud.dataflow.sdk.transforms.PTransform;
-import com.google.cloud.dataflow.sdk.transforms.ParDo;
-import com.google.cloud.dataflow.sdk.values.KV;
 import com.google.cloud.dataflow.sdk.values.PCollection;
 
-import java.io.IOException;
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -66,50 +56,30 @@ class GenomicPosition {
 	public String end = "";
 	public String ref = "";
 	public List<String> alt = new ArrayList<String>();
-	private int total = 0;
-	private String genotypeCounts = "";
-	public List<String> genotypes = new ArrayList<String>();
-	private List<Integer> counts = new ArrayList<Integer>();
-	public List<Float> probabilities = new ArrayList<Float>();
+	public double alleleFreq;
 	
 	// Constructor
 	GenomicPosition(String referenceName, String start, String end, String ref, 
-			String alt, String genotypeCounts, int total) {
-		this.referenceName = referenceName;
-		this.start = start;
+			String alt, double alleleFreq) {
+		this.referenceName = "chr" + referenceName;
+		this.start = String.valueOf(Integer.parseInt(start) - 1);
 		this.end = end;
 		this.ref = ref;
 		this.alt.add(alt);
-		this.genotypeCounts = genotypeCounts;
-		this.total = total;
-		try {
-			setDistributions();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+		//if (alleleFreq == null) {
+		//	alleleFreq = 0.0;
+		//}
+		this.alleleFreq = alleleFreq;
 	}
 	
 	// Print the position to stdout - debugging method
 	public void printPosition() {
-		String printString = referenceName + " " + start + " " + ref + " " + alt + " " + genotypeCounts;
-		String dists = referenceName + " " + start;
-		for (int i=0; i<genotypes.size(); i++) {
-			dists = dists + " " + genotypes.get(i) + ":" + counts.get(i) + ":" + probabilities.get(i);
-		}
+		String printString = referenceName + " " + start + " " + ref + " " + alt + " " + alleleFreq;
 		System.out.println(printString);
-		System.out.println(dists);
 	}
 	
 	public boolean isValid() {
-		if (!(genotypes.size() > 0)) {
-			return false;
-		} else if (!(probabilities.size() > 0)) {
-			return false;
-		} else if (!(counts.size() > 0)) {
-			return false;
-		} else if (genotypes.size() != probabilities.size()) {
-			return false;
-		} else if (ref == null) {
+		if (ref == null) {
 			return false;
 		} else if (alt == null) {
 			return false;
@@ -123,63 +93,82 @@ class GenomicPosition {
 		return true;
 	}
 	
-	// Set the genotype distributions based on the counts column from the input table
-	// Keep three lists of genotypes, counts, and probabilities.  Each index in each table
-	// corresponds with a genotype.
-	private void setDistributions() throws IOException {
-		  int found = 0;
-		  // Collect input
-	      for (String genotypeAndCount: genotypeCounts.split(";")){
-	    	  String[] parts = genotypeAndCount.split(":");
-	    	  genotypes.add(parts[0]);
-	    	  int count = Integer.parseInt(parts[1]);
-	    	  counts.add(count);
-	    	  found += count;
-	       }
-	      if (found < total) {
-	    	  genotypes.add(null);
-	    	  counts.add(total - found);
-	      } else if (total < found) {
-	    	  throw new IOException("Unexpected number of genotypes");
-	      }
-	      double previous = 0;
-	      for(int c : counts) {
-	    	  double probability =  ((double) c / (double) total);
-	    	  probabilities.add((float) (probability + previous));
-	    	  previous = previous + probability;
-	      }
+	public boolean isIndel() {
+		if (ref.length() > 1) {
+			return true;
+		} else if (alt.get(0).length() > 1) {
+			return true;
+		}
+		return false;
 	}
 	
+	public boolean isTransition() {
+		String alternate = alt.get(0);
+		if (ref.equals("A") && alternate.equals("G")) {
+			return true;
+		} else if (ref.equals("G") && alternate.equals("A")) {
+			return true;
+		} else if (ref.equals("C") && alternate.equals("T")) {
+			return true;
+		} else if (ref.equals("T") && alternate.equals("C")) {
+			return true;
+		}
+		return false;
+	}
+	
+	// Decide if this site should be a singleton
+	public boolean isSingleton(int genomeCount) {
+		if (alleleFreq >= 0.0005) {
+			return false;
+		}
+		
+		double veryRareVariants = 550000000.0; 
+		double expectedSingletons = 20000.0; 
+		double titv = 0.5;
+		if (isTransition()) {
+			titv = 2.0;
+
+		}
+		double pS = (genomeCount * expectedSingletons * titv) / veryRareVariants;	
+		double random = Math.random();
+		if (random <= pS) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+
 	// Randomly emit a genotype based on the probabilities observed for this position in the input table.
 	public List<String> emitGenotype() {
-		double random = Math.random();
 		List<String> randomGenotype = new ArrayList<String>();
-		for (int i=0; i<genotypes.size(); i++) {
-			if (random <= (probabilities.get(i))) {
-				if (genotypes.get(i) == null) {
-					return(randomGenotype);
-				}
-				String[] alleles = genotypes.get(i).split(",");
-				for (String a: alleles) {
-					randomGenotype.add(a);
-				}
-				break;
+		for (int i=0; i<2; i++) {
+			double random = Math.random();
+			if (random <= alleleFreq) {
+				randomGenotype.add("1");
+			} else {
+				randomGenotype.add("0");
 			}
 		}
 		return(randomGenotype);
 	}
-	
-	public List<String> returnGenotype(int index) {
-		if (genotypes.get(index) == null) {
-			List<String> genotype = new ArrayList<String>();
-			return genotype;
+
+	public List<String> emitSingleton() {
+		List<String> singleton = new ArrayList<String>();
+		for (int i=0; i<2; i++) {
+			singleton.add("1");
+			singleton.add("0");
 		}
-		String[] alleles = genotypes.get(index).split(",");
-		List<String> genotype = new ArrayList<String>();
-		for (String a: alleles) {
-			genotype.add(a);
+		return(singleton);
+	}
+		
+	public List<String> emitRef() {
+		List<String> ref = new ArrayList<String>();
+		for (int i=0; i<2; i++) {
+			ref.add("0");
+			ref.add("0");
 		}
-		return genotype;
+		return(ref);
 	}
 	
 	// Randomly generate an integer for the AD column
@@ -246,7 +235,7 @@ class GenomicPosition {
 	}
 	
 	// Randomly generate an integer between start and end
-	private int getRandomInteger(int start, int end){
+	public int getRandomInteger(int start, int end){
 		if (start > end) {
 			throw new IllegalArgumentException("Start cannot exceed End.");
 		}
@@ -270,14 +259,13 @@ public class GenomeGenerator {
 		      
 	  // Default options - may be set from command line 
 	  private static final String GENOTYPE_DISTRIBUTION_TABLE = 
-	      "gbsc-gcp-project-mvp:simulated_data.aaa_genotypte_distributions_brca1";
+			  "gbsc-gcp-project-mvp:simulated_data.tute_brca1";
+			  //"silver-wall-555:TuteTable.hg19";
 
 	  private static final String OUTPUT_TABLE = 
-		  "gbsc-gcp-project-mvp:simulated_data.default_output_500";
+		  "gbsc-gcp-project-mvp:simulated_data.default_output_tute_5";
 	  
-	  private static final int GENOME_COUNT = 460; // Need to put this in the table - hard coding for now.
-	  
-	  private static final int RANDOM_GENOME_COUNT = 500;
+	  private static final int RANDOM_GENOME_COUNT = 5;
 
 	  /* 
 	   * ProcessRowFn
@@ -296,36 +284,51 @@ public class GenomeGenerator {
 		/**
 		 * @param randomCount
 	     */
-		private boolean fastMode = false;
-		/**
-		 * @param randomCount
-	     */
- 	    public ProcessRowFn(int randomCount, boolean fastMode) {
+
+ 	    public ProcessRowFn(int randomCount) {
  	    	super();
 		    this.randomCount = randomCount;
-		    this.fastMode = fastMode;
  	    }
 	    @Override
 	    public void processElement(ProcessContext c) {
 	    	
 	      // Access information from the input row
 	      TableRow row = c.element();
-	      String referenceName = (String) row.get("reference_name");
-	      String start = (String) row.get("start");
-	      String end = (String) row.get("end");
-	      String ref = (String) row.get("reference_bases");
-	      String alt = (String) row.get("alts");
-	      String genotypeCounts = (String) row.get("counts");
+	      String referenceName = (String) row.get("Chr");
+	      String start = (String) row.get("Start");
+	      String end = (String) row.get("End");
+	      String ref = (String) row.get("Ref");
+	      String alt = (String) row.get("Alt");
+	      Double alleleFreq;
+	      if (row.get("X1000G_ALL") == null) {
+	    	  alleleFreq = 0.0;
+	      } else {
+	    	  alleleFreq = Double.parseDouble(row.get("X1000G_ALL").toString());
+	    	  //meanTemp =   Double.parseDouble(row.get("mean_temp" ).toString());
+	      }
 	      
 	      // Create a new position object with row information
-	      GenomicPosition position = new GenomicPosition(referenceName, start, end, ref, alt, genotypeCounts, GENOME_COUNT);
+	      GenomicPosition position = new GenomicPosition(referenceName, start, end, ref, alt, alleleFreq);
 	      
 	      // Generate genomic data for the requested number of genomes.  Store call-subrows for insertion into main row later
+	      //position.printPosition();
 	      if (!position.isValid()) {
 	    	  return;
 	      }
 	      
-	      TableRow newRow = generateRow(position, randomCount, fastMode);
+	      
+	      TableRow newRow = null;
+	      if (position.alleleFreq < 0.0005) {
+	    	  if (position.isSingleton(randomCount)) {
+	    		  newRow = generateSingletonRow(position, randomCount);
+	    	  } else {
+	    		  //System.out.println("too rare");
+	    		  return;
+	    	  }
+	      } else {
+	    	  //System.out.println("generating row");
+	    	  newRow = generateRow(position, randomCount);
+	      }
 	      if (newRow.size() == 0) {
 	    	  return;
 	      }
@@ -335,66 +338,79 @@ public class GenomeGenerator {
 
 	  } 
 	  
-	  private static TableRow generateRow(GenomicPosition position, int sampleCount, boolean fastMode) {
+	  private static TableRow generateRow(GenomicPosition position, int sampleCount) {
     	  List<TableRow> calls = new ArrayList<>();
     	  
-    	  
-    	  if (!fastMode) {
-    		  int altCount = 0;
-    		  for (int i=1; i <= sampleCount; i++) {
-    			  String call_set_name = "R" + String.format("%06d", i);
-    			  List<String> randomGenotype = position.emitGenotype();
-    			  if (randomGenotype != null && randomGenotype.contains("1")) {
-    				  altCount += 1;
-    			  }
-    			  TableRow call = new TableRow()
-    					  	.set("call_set_name", call_set_name)
-    					  	.set("genotype", randomGenotype)
-	      					.set("AD", position.randomAD())
-	      					.set("DP", position.randomDP())
-	      					.set("QUAL", position.randomQual())
-	      					.set("GQ", position.randomGQ())
-	      					.set("genotype_likelihood", position.randomLikelihood())
-	      					.set("FILTER", position.getFilter())
-	      					.set("QC", NullArray());
+    	  int altCount = 0;
+    	  for (int i=1; i <= sampleCount; i++) {
+    		  String call_set_name = "R" + String.format("%06d", i);
+    		  List<String> randomGenotype = position.emitGenotype();
+    		  if (randomGenotype != null && randomGenotype.contains("1")) {
+    			  altCount += 1;
+    		  }
+    		  TableRow call = new TableRow()
+    				  .set("call_set_name", call_set_name)
+    				  .set("genotype", randomGenotype)
+    				  .set("AD", position.fastAD())
+    				  .set("DP", position.fastDP())
+    				  .set("QUAL", position.fastQual())
+    				  .set("GQ", position.fastGQ())
+    				  .set("genotype_likelihood", position.fastLikelihood())
+    				  .set("FILTER", position.getFilter())
+    				  .set("QC", NullArray());
 	      
-    			  calls.add(call);
-    		  }
-    		  
-    		  // If no alternate alleles are generated, don't insert a row
-        	  if (altCount == 0) {
-        		  TableRow emptyRow = new TableRow();
-        		  return emptyRow;
-        	  }
-    	  } else {
-    		  int frequencyIndex = 0;
-    		  
-    		  for (int i=1; i <= sampleCount; i++) {	
-    			 if (i <= position.probabilities.get(frequencyIndex) * sampleCount) {
-       			  	String call_set_name = "R" + String.format("%06d", i);
-       			  	List<String> fastGenotype = position.returnGenotype(frequencyIndex);
-       			  	TableRow call = new TableRow()
-       					  	.set("call_set_name", call_set_name)
-       					  	.set("genotype", fastGenotype)
-   	      					.set("AD", position.fastAD())
-   	      					.set("DP", position.fastDP())
-   	      					.set("QUAL", position.fastQual())
-   	      					.set("GQ", position.fastGQ())
-   	      					.set("genotype_likelihood", position.fastLikelihood())
-   	      					.set("FILTER", position.getFilter())
-   	      					.set("QC", NullArray());
-       			  	
-       			  	calls.add(call);
-       			  	
-    			 } else if (frequencyIndex < position.probabilities.size() - 1) {
-    				 i--;
-    				 frequencyIndex++;
-    			 }
-    		  }
+    		  calls.add(call);
     	  }
     		  
-
-      
+    		  // If no alternate alleles are generated, don't insert a row
+    	  if (altCount == 0) {
+    		  TableRow emptyRow = new TableRow();
+    		  return emptyRow;
+    	  }
+    	  
+    	  // Define complete row
+    	  TableRow newRow = new TableRow()
+    			  .set("reference_name", position.referenceName)
+    			  .set("start", position.start)
+    			  .set("end", position.end)
+    			  .set("reference_bases", position.ref)
+    			  .set("alternate_bases", position.alt)
+    			  .set("quality", position.randomQual())
+    			  .set("QC", NullArray())
+    			  .set("names", NullArray())
+    			  .set("filter", NullArray())
+    			  .set("call", calls);
+    	  
+    	  return newRow;
+	  }
+	  
+	  private static TableRow generateSingletonRow(GenomicPosition position, int sampleCount) {
+    	  List<TableRow> calls = new ArrayList<>();
+    	  
+    	  String singletonSample = "R" + String.format("%06d", position.getRandomInteger(0, sampleCount));
+    	  
+    	  for (int i=1; i <= sampleCount; i++) {
+    		  String call_set_name = "R" + String.format("%06d", i);
+    		  List<String> genotype = new ArrayList<String>();
+    		  if (call_set_name == singletonSample) {
+    			  genotype = position.emitSingleton();
+    		  } else {
+    			  genotype = position.emitRef();
+    		  }
+    		  TableRow call = new TableRow()
+    				  .set("call_set_name", call_set_name)
+    				  .set("genotype", genotype)
+    				  .set("AD", position.randomAD())
+    				  .set("DP", position.randomDP())
+    				  .set("QUAL", position.randomQual())
+    				  .set("GQ", position.randomGQ())
+    				  .set("genotype_likelihood", position.randomLikelihood())
+    				  .set("FILTER", position.getFilter())
+    				  .set("QC", NullArray());
+    		  calls.add(call);
+    	  }
+    		  
+    	  
     	  // Define complete row
     	  TableRow newRow = new TableRow()
     			  .set("reference_name", position.referenceName)
@@ -417,20 +433,16 @@ public class GenomeGenerator {
 		/**
 		 * @param randomCount
 		 */
-		private boolean fastMode = false;
-		/**
-		 * @param randomCount
-		 */
-	 	public GenerateGenomes(int randomCount, boolean fastMode) {
+
+	 	public GenerateGenomes(int randomCount) {
 	 	    super();
 			this.randomCount = randomCount;
-			this.fastMode = fastMode;
 	 	}
 	    @Override
 	    public PCollection<TableRow> apply(PCollection<TableRow> rows) {
 
 	      PCollection<TableRow> results = rows.apply(
-	          ParDo.of(new ProcessRowFn(randomCount, fastMode)));
+	          ParDo.of(new ProcessRowFn(randomCount)));
 
 	      return results;
 	    }
@@ -455,10 +467,16 @@ public class GenomeGenerator {
 	    Integer getNewGenomeCount();
 		void setNewGenomeCount(Integer newGenomeCount);
 		
-	    @Description("Fast mode - genotypes are not as nicely distributed as normal.")
+		@Description("Subsetting query")
+		//@Default.String("SELECT * FROM [gbsc-gcp-project-mvp:simulated_data.tute_brca1] LIMIT 1000")
+	    String getSubsetQuery();
+		void setSubsetQuery(String subsetQuery);
+	
+		@Description("Whether to append to an existing BigQuery table.")
 		@Default.Boolean(false)
-	    Boolean getFastMode();
-		void setFastMode(Boolean fastMode);
+		boolean getAppendToTable();	
+		void setAppendToTable(boolean value);
+		
 	  }
 	  
 	  private static TableSchema getTableSchema() {
@@ -499,13 +517,26 @@ public class GenomeGenerator {
 
 	    Pipeline p = Pipeline.create(options);
 
-	    p.apply(BigQueryIO.Read.from(options.getInputTable()))
-	     .apply(new GenerateGenomes(options.getNewGenomeCount(), options.getFastMode()))
-	     .apply(BigQueryIO.Write
-	        .to(options.getOutputTable())
-	        .withSchema(getTableSchema())
-	        .withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
-	        .withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_TRUNCATE));
+	    String subsetQuery = options.getSubsetQuery();
+	    if (subsetQuery == null) {
+	    
+	    	p.apply(BigQueryIO.Read.from(options.getInputTable()))
+	    	.apply(new GenerateGenomes(options.getNewGenomeCount()))
+	    	.apply(BigQueryIO.Write
+	    			.to(options.getOutputTable())
+	    			.withSchema(getTableSchema())
+	    			.withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
+	    			.withWriteDisposition(BigQueryIO.Write.WriteDisposition.WRITE_TRUNCATE));
+	    } else {
+	    	p.apply(BigQueryIO.Read.fromQuery(subsetQuery))
+	    	.apply(new GenerateGenomes(options.getNewGenomeCount()))
+	    	.apply(BigQueryIO.Write
+	    			.to(options.getOutputTable())
+	    			.withSchema(getTableSchema())
+	    			.withCreateDisposition(BigQueryIO.Write.CreateDisposition.CREATE_IF_NEEDED)
+	    			.withWriteDisposition(options.getAppendToTable()
+	    					 ? BigQueryIO.Write.WriteDisposition.WRITE_APPEND : BigQueryIO.Write.WriteDisposition.WRITE_TRUNCATE));
+	    }
 
 	    p.run();
 	  }
